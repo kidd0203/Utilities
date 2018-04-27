70fa5e6d4a5e7a6fbd20e3b1abbecb71c427317c
Aaron Lord <aaron@lorddesign.net>
2018-04-27 09:00:15 -0700

    Sonar technical debt

--- ShippingTrackingUtilities/ShippingTrackingUtilies/ShippingTrackingException.cs
+++ ShippingTrackingUtilities/ShippingTrackingUtilies/ShippingTrackingException.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace ShippingTrackingUtilities
 {
+    [Serializable]
     public class ShippingTrackingException : Exception
     {
         public string ResponseXml { get; }
@@ -9,10 +11,19 @@ namespace ShippingTrackingUtilities
         public ShippingTrackingException(string message, string responseXml) : base(message)
         {
             ResponseXml = responseXml;
+            Data.Add(nameof(ResponseXml), responseXml);
         }
 
         public ShippingTrackingException(string message) : base(message)
         {
+        }
+
+        protected ShippingTrackingException (SerializationInfo info, StreamingContext context)
+            : base(info, context) { }
+            
+        public override string ToString()
+        {
+            return $"{nameof(ResponseXml)}: {ResponseXml}\r\n{base.ToString()}";
         }
     }
 }
--- ShippingTrackingUtilities/ShippingTrackingUtilies/UPSTracking.cs
+++ ShippingTrackingUtilities/ShippingTrackingUtilies/UPSTracking.cs
@@ -2,62 +2,68 @@
 using System.IO;
 using System.Net;
 using System.Text;
+using System.Threading;
 using System.Xml.Serialization;
+using ShippingTrackingUtilities.UPSTrackingResult;
 
 namespace ShippingTrackingUtilities
 {
     public class UPSTracking : ITrackingFacility
     {
-        string trackingNumber;
+        private readonly string trackingNumber;
 
         public UPSTracking(string trackingNumber)
         {
             this.trackingNumber = trackingNumber;
-            ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;
+            ServicePointManager.SecurityProtocol = (SecurityProtocolType) 3072;
         }
 
         public ShippingResult GetTrackingResult()
         {
-            ShippingResult shippingResult = new ShippingResult();
-            string shippingResultInString = GetTrackingInfoUPSInString();
+            var shippingResult = new ShippingResult();
+            var shippingResultInString = GetTrackingInfoUPSInString();
 
             if (shippingResultInString.Contains("<ResponseStatusDescription>Failure</ResponseStatusDescription>")
-                && shippingResultInString.Contains("<Error><ErrorSeverity>Hard</ErrorSeverity>"))
+                && shippingResultInString.Contains("<Error><ErrorSeverity>Hard</ErrorSeverity>") &&
+                !shippingResultInString.Contains("No tracking information available"))
             {
                 throw new ShippingTrackingException("UPS API ERROR", shippingResultInString);
             }
 
-            XmlSerializer serializer = new XmlSerializer(typeof(UPSTrackingResult.TrackResponse));
-            MemoryStream memStream = new MemoryStream(Encoding.UTF8.GetBytes(shippingResultInString));
+            var serializer = new XmlSerializer(typeof(TrackResponse));
+            var memStream = new MemoryStream(Encoding.UTF8.GetBytes(shippingResultInString));
 
-            UPSTrackingResult.TrackResponse resultingMessage = new UPSTrackingResult.TrackResponse();
+            var resultingMessage = new TrackResponse();
 
             if (memStream != null)
-                resultingMessage = (UPSTrackingResult.TrackResponse)serializer.Deserialize(memStream);
+            {
+                resultingMessage = (TrackResponse) serializer.Deserialize(memStream);
+            }
 
             shippingResult = UPSTrackingResultWrap(resultingMessage);
 
             return shippingResult;
-
         }
 
         private string UPSRequest(string url, string requestText)
-        {   // try request upto 3 times 
-            string result = "";
-            ASCIIEncoding encodedData = new ASCIIEncoding();
-            byte[] byteArray = encodedData.GetBytes(requestText);
+        {
+            // try request upto 3 times 
+            var result = "";
+            var encodedData = new ASCIIEncoding();
+            var byteArray = encodedData.GetBytes(requestText);
 
 
-            int call_tries = 0;
-            int call_max = 3;
-            bool call_succeeded = false;
+            var call_tries = 0;
+            var call_max = 3;
+            var call_succeeded = false;
 
             while (call_tries < call_max && call_succeeded == false)
-            {  // try three times before giving up
+            {
+                // try three times before giving up
                 call_tries++;
                 try
                 {
-                    HttpWebRequest wr = (HttpWebRequest)WebRequest.Create(url);
+                    var wr = (HttpWebRequest) WebRequest.Create(url);
                     wr.Method = "POST";
                     wr.KeepAlive = false;
                     wr.UserAgent = "Benz";
@@ -65,13 +71,13 @@ namespace ShippingTrackingUtilities
                     wr.ContentLength = byteArray.Length;
 
                     // send xml data
-                    Stream SendStream = wr.GetRequestStream();
+                    var SendStream = wr.GetRequestStream();
                     SendStream.Write(byteArray, 0, byteArray.Length);
                     SendStream.Close();
 
                     // get da response
-                    HttpWebResponse WebResp = (HttpWebResponse)wr.GetResponse();
-                    using (StreamReader sr = new StreamReader(WebResp.GetResponseStream()))
+                    var WebResp = (HttpWebResponse) wr.GetResponse();
+                    using (var sr = new StreamReader(WebResp.GetResponseStream()))
                     {
                         result = sr.ReadToEnd();
                         sr.Close();
@@ -83,7 +89,7 @@ namespace ShippingTrackingUtilities
                 }
                 catch (Exception e)
                 {
-                    System.Threading.Thread.Sleep(200);
+                    Thread.Sleep(200);
                 }
             }
 
@@ -92,18 +98,17 @@ namespace ShippingTrackingUtilities
 
         private string GetTrackingInfoUPSInString()
         {
-            string apiUrl = "https://www.ups.com/ups.app/xml/Track"; // "https://wwwcie.ups.com/ups.app/xml/Track";
+            var apiUrl = "https://www.ups.com/ups.app/xml/Track"; // "https://wwwcie.ups.com/ups.app/xml/Track";
 
-            string xml1 = "<?xml version=\"1.0\"?>" +
-            "<AccessRequest xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">" +
-            @"	<Password>%PASSWORD%</Password>
+            var xml1 = "<?xml version=\"1.0\"?>" +
+                       "<AccessRequest xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">" +
+                       @"	<Password>%PASSWORD%</Password>
                 <UserId>%USERID%</UserId>
             <AccessLicenseNumber>%ACCESSLICENSENUMBER%</AccessLicenseNumber>
             </AccessRequest>";
 
 
-
-            string xml2 = @"<?xml version='1.0'?>
+            var xml2 = @"<?xml version='1.0'?>
             <TrackRequest>
                 <Request>
                     <TransactionReference>
@@ -119,7 +124,7 @@ namespace ShippingTrackingUtilities
             //    apiUrl, 
             //    xml1.Replace("%PASSWORD%", ConnectionString.UPS_PASSWORD).Replace("%USERID%",ConnectionString.UPS_USER_ID).Replace("%ACCESSLICENSENUMBER%",ConnectionString.UPS_ACCESS_LICENSE_NO)
             //    + xml2.Replace("%TN%", trackingNumber));
-            string raw_response = UPSRequest(
+            var raw_response = UPSRequest(
                 apiUrl,
                 xml1.Replace("%ACCESSLICENSENUMBER%", ConnectionString.UPS_ACCESS_LICENSE_NO)
                 + xml2.Replace("%TN%", trackingNumber));
@@ -127,37 +132,47 @@ namespace ShippingTrackingUtilities
             return raw_response;
         }
 
-        private ShippingResult UPSTrackingResultWrap(UPSTrackingResult.TrackResponse resultingMessage)
+        private ShippingResult UPSTrackingResultWrap(TrackResponse resultingMessage)
         {
-            ShippingResult shippingResult = new ShippingResult();
+            var shippingResult = new ShippingResult();
 
-            UPSTrackingResult.TrackResponseResponse response = (UPSTrackingResult.TrackResponseResponse)resultingMessage.Items[0];
+            var response = (TrackResponseResponse) resultingMessage.Items[0];
             if (response.ResponseStatusCode == "1")
             {
-                UPSTrackingResult.TrackResponseShipment shipment = (UPSTrackingResult.TrackResponseShipment)resultingMessage.Items[1];
+                var shipment = (TrackResponseShipment) resultingMessage.Items[1];
                 shippingResult.ServiceType = shipment.Service[0].Description;
                 shippingResult.StatusCode = shipment.Package[0].Activity[0].Status[0].StatusType[0].Code;
                 shippingResult.Status = shipment.Package[0].Activity[0].Status[0].StatusType[0].Description;
 
                 if (!string.IsNullOrEmpty(shippingResult.StatusCode))
+                {
                     if (shippingResult.StatusCode == "D")
                     {
                         shippingResult.Delivered = true;
                         shippingResult.StatusSummary = shipment.Package[0].Activity[0].ActivityLocation[0].Description;
-                        shippingResult.DeliveredDateTime = shipment.Package[0].Activity[0].Date + " " + shipment.Package[0].Activity[0].Time;
-                        shippingResult.SignatureName = string.IsNullOrEmpty(shipment.Package[0].Activity[0].ActivityLocation[0].SignedForByName) ? "" : shipment.Package[0].Activity[0].ActivityLocation[0].SignedForByName;
+                        shippingResult.DeliveredDateTime =
+                            shipment.Package[0].Activity[0].Date + " " + shipment.Package[0].Activity[0].Time;
+                        shippingResult.SignatureName =
+                            string.IsNullOrEmpty(shipment.Package[0].Activity[0].ActivityLocation[0].SignedForByName)
+                                ? ""
+                                : shipment.Package[0].Activity[0].ActivityLocation[0].SignedForByName;
                     }
+                }
 
 
                 if (!string.IsNullOrEmpty(shippingResult.StatusCode))
+                {
                     if (shippingResult.StatusCode != "D")
                     {
                         shippingResult.PickupDate = shipment.PickupDate;
                         shippingResult.ScheduledDeliveryDate = shipment.ScheduledDeliveryDate;
 
                         if (shipment.Package[0].Message != null)
+                        {
                             shippingResult.Message = shipment.Package[0].Message[0].Description;
+                        }
                     }
+                }
             }
             else
             {
